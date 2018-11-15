783826db3997379e527f3553a40ac668f189bbf6
git <102123439@qq.com>
2018-11-15 11:27:18 +0800

    mysite项目第四次提交

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,7 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
 """在这个简单的投票应用中，需要创建两个模型：问题 Question 和选项 Choice。Question 模型包括问题描述和发布时间。Choice 模型有两个字段，选项描述
 和当前得票数。每个选项属于一个问题。
 """
@@ -8,10 +9,36 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 class Choice(models.Model):
     question = models.ForeignKey(Question,on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+
+class Blog(models.Model):
+    name = models.CharField(max_length=100)
+    tagline = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Author(self):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+
+    def __str__(self):
+        return  self.name
+
+class Entry(self):
+    
 
 """
 代码非常直白。每个模型被表示为 django.db.models.Model 类的子类。每个模型有一些类变量，它们都表示模型里的一个数据库字段。
