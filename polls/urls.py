988570cf10384fe067e5ce76f9fc9e4bf0608b5a
git <102123439@qq.com>
2018-11-15 15:59:25 +0800

    mysite项目创建

--- polls/urls.py
+++ polls/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
 # ex: /polls/5/results/
     path('<int:question_id>/results/',views.results,name='results'),
 # ex: /polls/5/vote/
-    path('<int:question_id>/votes',views.vote,name='vote'),
+    path('<int:question_id>/vote',views.vote,name='vote'),
 ]
 
 """
